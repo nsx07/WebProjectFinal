7e5b60b337200d50bf87ba3a6f78ea4d98920fc3
nsx07 <97983021+nsx07@users.noreply.github.com>
2022-11-05 14:46:50 -0300

    html model and banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './components/components/core/core.component';
@@ -12,7 +12,8 @@ import { CoreComponent } from './components/components/core/core.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/components/core/core.component.ts
+++ src/app/components/components/core/core.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-core',
@@ -9,7 +10,49 @@ export class CoreComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public matrix = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ]
+
+  public markerTurn! : string
+
+  public movements = {
+    option : this.markerTurn == 'X' ?  true : false ,
+    putX :  'fa-solid fa-xmark fa-4x',
+    putO :  'fa-regular fa-circle fa-3x'
   }
 
+  public insertIn = {
+    AA : (...rest : any[]) => {
+      this.matrix[0][0] = 1
+    },
+    AB : (...rest : any[]) => {
+      this.matrix[0][1] = 1
+    },
+    AC : (...rest : any[]) => {
+      this.matrix[0][2] = 1
+    },
+    BA : (...rest : any[]) => {
+      this.matrix[1][0] = 1
+    },
+    BB : (...rest : any[]) => {
+      this.matrix[1][1] = 1
+    },
+    BC : (...rest : any[]) => {
+      this.matrix[1][2]
+    },
+    CA : (...rest : any[]) => {
+      this.matrix[2][0] = 1
+    },
+    CB : (...rest : any[]) => {
+      this.matrix[2][1]
+    },
+    CC : (...rest : any[]) => {
+      this.matrix[2][2]
+    },
+  }
 }
