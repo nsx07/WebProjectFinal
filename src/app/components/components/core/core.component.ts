0037a34ef775e6b2a0d7ffbeed9b971bc8ecaee4
Felipe Pires <felipeaugustodluz@hotmail.com>
2022-11-18 02:06:39 -0300

    complete

--- src/app/components/components/core/core.component.ts
+++ src/app/components/components/core/core.component.ts
@@ -6,10 +6,11 @@ import { TitleStrategy } from '@angular/router';
   templateUrl: './core.component.html',
   styleUrls: ['./core.component.scss']
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent {
 
-  public playerTurn = true;
-  public status = {
+  lastPlay : any[] = [];
+  playerTurn = true;
+  status = {
     on : false,
     win : false,
     tie : false,
@@ -18,55 +19,77 @@ export class CoreComponent implements OnInit {
   }
 
   constructor() { }
-  ngOnInit(): void {}
 
-  public matrix : any[] = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
+  matrix : any[] = [
+    null,null,null,
+    null,null,null,
+    null,null,null
   ]
 
-  public insertIn = {
-    put : (rest : any[], id : string) => {
-      if (!this.matrix[rest[0]][rest[1]]) {
-        this.matrix[rest[0]][rest[1]] = this.playerTurn ? 1 : 0;
-        const element : HTMLElement = document.getElementById(id) || document.createElement('div');
-        element.innerHTML = this.playerTurn ? '<i class="fa-solid fa-xmark fa-4x"></i>' : '<i class="fa-regular fa-circle fa-3x"></i>'
-        this.playerTurn = !this.playerTurn
-        const reload = this.win() || this.tie() ? setInterval(()=> {
-          this.status.reload ? location.reload() : 0
-        }, 500) : 0
-        if (this.win()) {
-          this.status.on = true
-          this.status.win = true
-        } else if (this.tie() ){
-          this.status.on = true
-          this.status.tie = true
-          this.status.win = false
-        console.log(this.status, this.tie(), this.win())
+  reload() {
+    this.status.reload = false
+    this.status.on = false
+    this.status.tie = false
+    this.status.win = false
+    setTimeout(() => this.refresh(), 500)
+  }
+
+  insertIn (loc : number) {
+    if (this.matrix[loc] === null) {
+      this.matrix[loc] = this.playerTurn ? 1 : 0;
+      const element = (document.getElementById(loc.toString()) || document.createElement('div'))
+          .innerHTML = this.playerTurn
+          ? '<i class="fa-solid fa-xmark fa-4x"></i>'
+          : '<i class="fa-regular fa-circle fa-3x"></i>'
+
+      this.playerTurn = !this.playerTurn
+      this.lastPlay = [loc, loc.toString()]
+      if (this.win()) {
+        this.status.on = true
+        this.status.win = true
+      } else if (this.tie() ){
+        this.status.on = true
+        this.status.tie = true
+        this.status.win = false
       }
     }
+  }
+
+  undo() {
+      let loc = this.lastPlay[0]
+      this.matrix[loc] = null
+      const element = (document.getElementById(this.lastPlay[1]) || document.createElement('div')).innerHTML = "";
+  }
+
+  refresh() {
+    for ( let i = 0; i < this.matrix.length; i++)
+    {
+      this.matrix[i] = null
+      let element = (document.getElementById(i.toString()) || document.createElement('div')).innerHTML = ""
     }
   }
 
-  public tie() {
-    let arr : any[] = []
-    this.matrix.forEach((row) => arr.push(row.filter((item : any ) => item === null)))
-    arr = arr.flatMap(num => num)
-    return arr.includes(null) ? false : true
+  tie() : boolean {
+    return this.matrix.includes(null) ? false : true
   }
 
-  public win() : boolean {
+  win() : boolean {
+    let matrix = [
+      [this.matrix[0], this.matrix[1], this.matrix[2]],
+      [this.matrix[3], this.matrix[4], this.matrix[5]],
+      [this.matrix[6], this.matrix[7], this.matrix[8]]
+    ]
+
     const mainDiagonal : any[] = []
     const secDiagonal : any[] = []
-    for (let i = 0; i < this.matrix.length; i++) {
-      const linha : any[] = this.matrix[i]
+    for (let i = 0; i < matrix.length; i++) {
+      const linha : any[] = matrix[i]
       const remainRow = linha.filter((item => item === linha[0] && item !== null))
       let colum : any[] = []
-      for (let j = 0; j < 3; j++) colum.push(this.matrix[j][i])
+      for (let j = 0; j < 3; j++) colum.push(matrix[j][i])
       const remainColum = colum.filter((item => item === colum[0] && item !== null))
-      mainDiagonal.push(this.matrix[i][i])
-      secDiagonal.push(this.matrix[i][this.matrix.length-1-i])
+      mainDiagonal.push(matrix[i][i])
+      secDiagonal.push(matrix[i][matrix.length-1-i])
       if (remainRow.length === 3 ) {
         this.status.winner = remainRow[0]
         return true
@@ -85,6 +108,5 @@ export class CoreComponent implements OnInit {
       return true
     }
     return false
-    // return remainDiagMain.length === 3 || remainDiagSec.length === 3 ? true : false
   }
 }
